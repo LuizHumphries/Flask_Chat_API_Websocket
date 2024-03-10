f43493c259d109c4b8e8b74df437198ed0b658d7
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-03-10 15:58:04 -0300

    App created, Socket io implemented

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def render_initial_page():
     return render_template("index.html")
 
+@socketio.on("message")
+def handle_message(message):
+    emit("message", f"Client Message: {message}", broadcast=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
